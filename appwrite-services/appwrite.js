2a026d0fe596b8e5ef24339802f5eed7c7670389
avishek15 <avishekalex15@gmail.com>
2024-09-16 23:31:03 +0800

    env vars

--- appwrite-services/appwrite.js
+++ appwrite-services/appwrite.js
@@ -26,7 +26,19 @@ export async function createSessionClient() {
 }
 
 export async function createAdminClient() {
-  const client = new Client()
+  const client = new Client();
+  if (!process.env.APPWRITE_ENDPOINT) {
+    throw new Error("Environment variable APPWRITE_ENDPOINT is not defined");
+  }
+
+  if (!process.env.APPWRITE_PROJECT_ID) {
+    throw new Error("Environment variable APPWRITE_PROJECT_ID is not defined");
+  }
+
+  if (!process.env.APPWRITE_API_KEY) {
+    throw new Error("Environment variable APPWRITE_API_KEY is not defined");
+  }
+  client
     .setEndpoint(process.env.APPWRITE_ENDPOINT)
     .setProject(process.env.APPWRITE_PROJECT_ID)
     .setKey(process.env.APPWRITE_API_KEY);
