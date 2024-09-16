9a0b039f328f84d0869983ecae804245073725e3
avishek15 <avishekalex15@gmail.com>
2024-09-16 21:49:59 +0800

    added response to api

--- app/api/create-table/route.js
+++ app/api/create-table/route.js
@@ -1,6 +1,13 @@
 import { createSessionClient } from "@/appwrite-services/appwrite";
+import { NextResponse } from "next/server";
 
 export async function GET() {
-  const { database } = createSessionClient();
-  console.log(database);
+  try {
+    const { database } = createSessionClient();
+    console.log(database);
+    return NextResponse.next();
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: error }, { status: 500 });
+  }
 }
